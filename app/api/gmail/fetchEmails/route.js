d60123b265e64b1e1a6e923785e8d099c692f674
Shivam Stutya <shivam.s@iouring.com>
2025-08-04 19:23:28 +0530

    error fix

--- app/api/gmail/fetchEmails/route.js
+++ app/api/gmail/fetchEmails/route.js
@@ -3,7 +3,7 @@
 import { google } from 'googleapis';
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../../auth/[...nextauth]/route';
+import { authOptions } from '../../auth/authOptions';
 
 async function getEmailBody(message) {
   try {
